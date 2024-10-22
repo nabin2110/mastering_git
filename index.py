4292b5ec5c8b08815ef64d8cf08370ee7ea619e0
nabin2110 <nabinthapamessifan@gmail.com>
2024-10-22 11:31:52 +0545

    index.py added

---
+++ index.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self,model,brand,price,launch_date):
+        self.model = model
+        self.brand = brand
+        self.price = price
+        self.launch_date = launch_date
+
+    def get_car_detail(self):
+        pass
