fda4b3bc7a69cdf688b54435c8102b7fc19cb1f8
nabin2110 <nabinthapamessifan@gmail.com>
2024-10-22 11:35:14 +0545

    Object of the car has been instantiated

--- index.py
+++ index.py
@@ -6,4 +6,8 @@ class Car:
         self.launch_date = launch_date
 
     def get_car_detail(self):
-        pass
+        print(f"the model of the car is {self.model} and the brand of the car is {self.brand} and the price of the car is {self.price} and it's launch date on {self.launch_date}")
+
+
+toyota = Car(model="toyota",brand="toyota",price=200000,launch_date="2022/01/12")
+toyota.get_car_detail()
